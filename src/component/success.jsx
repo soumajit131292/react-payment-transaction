68089dcb0170382fb54abd3417ad391c777cee48
soumajit131292 <soumajit131292@gmail.com>
2019-12-09 14:33:06 +0530

    timeout features added

--- src/component/success.jsx
+++ src/component/success.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 import {withRouter} from 'react-router-dom'
 class Success extends Component {
+    componentDidMount(){
+        window.setTimeout(() => {
+            this.props.history.push('/');
+         }, 6000)
+     
+    }
     render() {
         return (
             <div>
-                success
+                <h3>Payment is done successfully.</h3>
+                <h5>Redirecting you to homepage.....</h5>
             </div>
         )
     }
